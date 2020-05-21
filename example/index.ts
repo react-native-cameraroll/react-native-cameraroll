92e1954f6f71de5b12a95f94235b89b9d127cb17
Harry Yu <hy.harry.yu@gmail.com>
2020-05-21 10:40:30 -0700

    Fixed flow checking by converting index.ts to Typescript

--- example/index.ts
+++ example/index.ts
@@ -3,9 +3,6 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
- * @format
- * @flow
  */
 import {AppRegistry} from 'react-native';
 import App from './App';
