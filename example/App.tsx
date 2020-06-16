4da8310892cf2188ead33260c157682944edd7e9
Bartol Karuza <bartol.k@gmail.com>
2020-06-16 13:20:51 +0300

    feat: Added `include` parameter to getPhotos to let users tradeoff performance by omitting metadata (#178) (#200)
    BREAKING CHANGE: meta data is no longer added as default. This applies to `fileName`, `fileSize` and `location`. If you need this metadata use the new `include` parameter. Adding this metadata will slow down the `getPhotos` function, so consider only retrieving this meta data for smaller sets of images as needed, instead of for all images.
    
    * Created getPhotosFast function, fixed inconsistent getPhotos toDate logic
    
    * Fixed wrong param name, added better typechecking
    
    * Added example, renamed allowEmptyFilenames to skipGettingFilenames
    
    * Removed `after` from getPhotosFast docs
    
    * Redid implementation based on a new `include` param
    
    * Updated API to use include parameter
    
    * Fixed flow checking by converting index.ts to Typescript
    
    * Unformatted README.md
    
    * Unformatted README.md
    
    * Unformatted README.md
    
    * Added .prettierignore and ignored README.md for now
    
    * Made example/index.js not checked by Flow
    
    * Updated README.md to include notes in the outputs too
    
    * Made inclusion of fields consistent, addressed other feedback
    
    * Renamed GetPhotosFastParams back to GetPhotosParams
    
    * Updated documentation to reflect nullable types
    
    * Updated typings and documentation for fromTime, toTime
    
    * Updated to fix `hasNextPage` being incorrectly false in some cases
    
    Co-authored-by: Harry Yu <hy.harry.yu@gmail.com>

--- example/App.tsx
+++ example/App.tsx
@@ -6,8 +6,11 @@
  *
  * @format
  */
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React, {Component} from 'react';
+import ExampleContainer from './js/ExampleContainer';
 
-AppRegistry.registerComponent(appName, () => App);
+export default class App extends Component {
+  render() {
+    return <ExampleContainer />;
+  }
+}
