daa6a9faebe9099a7f6b764b84772eff1a7d4ad8
Chris Bobbe <csbobbe@gmail.com>
2021-03-20 23:55:20 -0400

    chore: Support React Native v0.63. (#264)
    Co-authored-by: Jesse Katsumata <niconico.clarinet@gmail.com>

--- example/index.js
+++ example/index.js
@@ -1,11 +1,7 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
  * @format
  */
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
