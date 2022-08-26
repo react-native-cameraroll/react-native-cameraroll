6962577773e226187325840c8030fdb804273c54
Sparsha Saha <saha.sparsha@gmail.com>
2022-08-26 13:54:24 +0530

    chore(test): added unit testing for iOS permissions (#418)
    Co-authored-by: Sparsha Saha <sparshasaha@Sparshas-MacBook-Pro.local>

---
+++ src/__tests__/CameraRollIOSPermissionTest.ts
@@ -0,0 +1,66 @@
+let mockCheckPermission: jest.Mock;
+let mockRequestReadWritePermission: jest.Mock;
+let mockRequestAddOnlyPermission: jest.Mock;
+let mockRefreshPhotoSelection: jest.Mock;
+
+jest.mock('../cameraRollPermissionNativeInterface', () => {
+  mockCheckPermission = jest.fn();
+  mockRequestReadWritePermission = jest.fn();
+  mockRequestAddOnlyPermission = jest.fn();
+  mockRefreshPhotoSelection = jest.fn();
+  return {
+    checkPermission: mockCheckPermission,
+    requestReadWritePermission: mockRequestReadWritePermission,
+    requestAddOnlyPermission: mockRequestAddOnlyPermission,
+    refreshPhotoSelection: mockRefreshPhotoSelection,
+  };
+});
+
+jest.mock('react-native', () => {
+  return {
+    NativeEventEmitter: jest.fn().mockImplementation(() => {
+      return {
+        removeAllListeners: () => {
+          console.log;
+        },
+      };
+    }),
+    Platform: {
+      OS: 'ios',
+    },
+  };
+});
+
+import {
+  iosReadGalleryPermission,
+  iosRequestReadWriteGalleryPermission,
+  iosRequestAddOnlyGalleryPermission,
+  iosRefreshGallerySelection,
+} from '../CameraRollIOSPermission';
+
+describe('CameraRollIosPermission', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Should call checkPermission if iosReadGalleryPermission is called', () => {
+    iosReadGalleryPermission('addOnly');
+    expect(mockCheckPermission).toHaveBeenCalledTimes(1);
+    expect(mockCheckPermission).toHaveBeenCalledWith('addOnly');
+  });
+
+  it('Should call requestReadWritePermission if iosRequestReadWriteGalleryPermission is called', () => {
+    iosRequestReadWriteGalleryPermission();
+    expect(mockRequestReadWritePermission).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should call requestAddOnlyPermission if iosRequestAddOnlyGalleryPermission is called', () => {
+    iosRequestAddOnlyGalleryPermission();
+    expect(mockRequestAddOnlyPermission).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should call refreshPhotoSelection if iosRefreshGallerySelection is called', () => {
+    iosRefreshGallerySelection();
+    expect(mockRefreshPhotoSelection).toHaveBeenCalledTimes(1);
+  });
+});
