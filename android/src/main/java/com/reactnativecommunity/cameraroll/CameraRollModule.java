ce4e2660a8cf2598cad5f52a45eb940cd0b177e3
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-02-02 19:00:30 +0100

    feat: change java files to apply for turboModule

--- android/src/main/java/com/reactnativecommunity/cameraroll/CameraRollModule.java
+++ android/src/main/java/com/reactnativecommunity/cameraroll/CameraRollModule.java
@@ -33,7 +33,6 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
@@ -64,7 +63,7 @@ import javax.annotation.Nullable;
  * {@link MediaStore.Images}).
  */
 @ReactModule(name = CameraRollModule.NAME)
-public class CameraRollModule extends ReactContextBaseJavaModule {
+public class CameraRollModule extends NativeCameraRollModuleSpec {
 
   public static final String NAME = "RNCCameraRoll";
 
