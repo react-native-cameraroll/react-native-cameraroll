286b300363ccf7c44d352cf9d42da48e21045c4d
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-02-06 18:34:03 +0100

    feat: update TS specs

--- src/NativeCameraRollModule.ts
+++ src/NativeCameraRollModule.ts
@@ -1,56 +1,60 @@
 import {
-    // @ts-ignore - remove this comment when RN in the repo & example app is upgraded
-    TurboModuleRegistry,
-    // @ts-ignore - remove this comment when RN in the repo & example app is upgraded
-    TurboModule,
+  // @ts-ignore - remove this comment when RN in the repo & example app is upgraded
+  TurboModuleRegistry,
+  // @ts-ignore - remove this comment when RN in the repo & example app is upgraded
+  TurboModule,
 } from 'react-native';
-  
+
 type Album = {
-    title: string;
-    count: number;
+  title: string;
+  count: number;
 };
 
-export type PhotoIdentifier = {
-    node: {
-      type: string;
-      group_name: string;
-      image: {
-        filename?: string;
-        filepath?: string;
-        extension?: string;
-        uri: string;
-        height: number;
-        width: number;
-        fileSize?: number;
-        playableDuration: number;
-        orientation?: number;
-      };
-      timestamp: number;
-      location?: {
-        latitude?: number;
-        longitude?: number;
-        altitude?: number;
-        heading?: number;
-        speed?: number;
-      };
+type PhotoIdentifier = {
+  node: {
+    type: string;
+    group_name: string;
+    image: {
+      filename: string | null;
+      filepath: string | null;
+      extension: string | null;
+      uri: string;
+      height: number;
+      width: number;
+      fileSize: number | null;
+      playableDuration: number;
+      orientation: number | null;
     };
+    timestamp: number;
+    location: {
+      latitude?: number;
+      longitude?: number;
+      altitude?: number;
+      heading?: number;
+      speed?: number;
+    } | null;
   };
-  
-export type PhotoIdentifiersPage = {
-    edges: Array<PhotoIdentifier>;
-    page_info: {
-      has_next_page: boolean;
-      start_cursor?: string;
-      end_cursor?: string;
-    };
-    limited?: boolean;
+};
+
+type PhotoIdentifiersPage = {
+  edges: Array<PhotoIdentifier>;
+  page_info: {
+    has_next_page: boolean;
+    start_cursor?: string;
+    end_cursor?: string;
+  };
+  limited?: boolean;
 };
 
 export interface Spec extends TurboModule {
-    saveToCameraRoll(uri: string, options: Object): Promise<string>;
-    getPhotos(params: Object): Promise<PhotoIdentifiersPage>; 
-    getAlbums(params: Object): Promise<Album[]>;
-    deletePhotos(photoUris: Array<string>): Promise<void>;
+  saveToCameraRoll(uri: string, options: Object): Promise<string>;
+  getPhotos(params: Object): Promise<PhotoIdentifiersPage>;
+  getAlbums(params: Object): Promise<Album[]>;
+  deletePhotos(photoUris: Array<string>): Promise<void>;
+  getPhotoByInternalID(
+    internalID: string,
+    options: Object,
+  ): Promise<PhotoIdentifier>;
 }
-  
+
 export default TurboModuleRegistry.getEnforcing<Spec>('RNCCameraRoll');
--- src/NativeCameraRollPermissionModule.ts
+++ src/NativeCameraRollPermissionModule.ts
@@ -1,17 +1,27 @@
 import {
-    TurboModuleRegistry,
-    TurboModule,
-  } from 'react-native';
-import type { Int32 } from 'react-native/Libraries/Types/CodegenTypes';
-  
+  // @ts-ignore - remove this comment when RN in the repo & example app is upgraded
+  TurboModuleRegistry,
+  // @ts-ignore - remove this comment when RN in the repo & example app is upgraded
+  TurboModule,
+} from 'react-native';
+import type {Int32} from 'react-native/Libraries/Types/CodegenTypes';
+
+type CameraRollAuthorizationStatus =
+  | 'granted'
+  | 'limited'
+  | 'denied'
+  | 'unavailable'
+  | 'blocked'
+  | 'not-determined';
+
 export interface Spec extends TurboModule {
-    checkPermission(content: string): Promise<string>;
-    requestReadWritePermission(): Promise<string>;
-    requestAddOnlyPermission(): Promise<string>;
-    refreshPhotoSelection(): Promise<string>;
-    addListener(eventName: string): void;
-    removeListeners(count: Int32): void;
+  checkPermission(content: string): Promise<CameraRollAuthorizationStatus>;
+  requestReadWritePermission(): Promise<CameraRollAuthorizationStatus>;
+  requestAddOnlyPermission(): Promise<CameraRollAuthorizationStatus>;
+  refreshPhotoSelection(): Promise<boolean>;
+  addListener(eventName: string): void;
+  removeListeners(count: Int32): void;
 }
-  
+
 // we call get here since on Android this module does not exist and it would throw
 export default TurboModuleRegistry.get<Spec>('RNCCameraRollPermission')!;
