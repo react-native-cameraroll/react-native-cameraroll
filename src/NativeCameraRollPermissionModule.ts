5408d836ed1e148a1af5ce327f04ba44cbf0b2ed
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-04-23 14:56:03 +0200

    chore: prepare library for 0.74 (#603)
    * chore: bump FabricExample to RN 74

--- src/NativeCameraRollPermissionModule.ts
+++ src/NativeCameraRollPermissionModule.ts
@@ -1,5 +1,5 @@
 import {TurboModuleRegistry, TurboModule} from 'react-native';
-import type {Int32} from 'react-native/Libraries/Types/CodegenTypes';
+import type {Double} from 'react-native/Libraries/Types/CodegenTypes';
 
 type CameraRollAuthorizationStatus =
   | 'granted'
@@ -15,7 +15,7 @@ export interface Spec extends TurboModule {
   requestAddOnlyPermission(): Promise<CameraRollAuthorizationStatus>;
   refreshPhotoSelection(): Promise<boolean>;
   addListener(eventName: string): void;
-  removeListeners(count: Int32): void;
+  removeListeners(count: Double): void;
 }
 
 // we call get here since on Android this module does not exist and it would throw
