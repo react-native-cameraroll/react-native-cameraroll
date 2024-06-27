b250afb6ed259e72dfd26fe47496e455fa674196
Arjan Zuidema <arjan@myalbum.com>
2024-06-27 15:40:20 +0200

    chore(example): Bumped react-native version to 0.74.2 (#641)
    * Bumped react-native version to 0.74.2
    
    * Updated permissions in AndroidManifest
    
    * Check android permissions on startup

--- FabricExample/App.tsx
+++ FabricExample/App.tsx
@@ -6,11 +6,63 @@
  *
  * @format
  */
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import ExampleContainer from './js/ExampleContainer';
+import {PermissionsAndroid, Platform} from 'react-native';
 
-export default class App extends Component {
-  render() {
-    return <ExampleContainer />;
+export default function App() {
+  async function hasAndroidPermission() {
+    const getCheckPermissionPromise = () => {
+      if ((Platform.Version as number) >= 33) {
+        return Promise.all([
+          PermissionsAndroid.check(
+            PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+          ),
+          PermissionsAndroid.check(
+            PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+          ),
+        ]).then(
+          ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+            hasReadMediaImagesPermission && hasReadMediaVideoPermission,
+        );
+      } else {
+        return PermissionsAndroid.check(
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        );
+      }
+    };
+
+    const hasPermission = await getCheckPermissionPromise();
+    if (hasPermission) {
+      return true;
+    }
+    const getRequestPermissionPromise = () => {
+      if ((Platform.Version as number) >= 33) {
+        return PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+        ]).then(
+          statuses =>
+            statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+              PermissionsAndroid.RESULTS.GRANTED &&
+            statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+              PermissionsAndroid.RESULTS.GRANTED,
+        );
+      } else {
+        return PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        ).then(status => status === PermissionsAndroid.RESULTS.GRANTED);
+      }
+    };
+
+    return await getRequestPermissionPromise();
   }
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      hasAndroidPermission();
+    }
+  });
+
+  return <ExampleContainer />;
 }
